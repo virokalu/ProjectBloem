c294ea9f695f00707eac2426ca0779b9f57f752c
manoj <manojlakshan9991@gmail.com>
2023-02-12 13:02:14 -0800

    add device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:project_bloem/screens/welcome_screen/welcome_screen.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  return runApp( const MaterialApp(home: MyApp()), // use MaterialApp
-  );
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context) => MyApp(),
+  ));
 }
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return const WelcomeScreen();
-
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blue,
+      ),
+      home : WelcomeScreen(),
+    );
   }
 }
