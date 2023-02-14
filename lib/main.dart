3efcd25f8fb854c46ca20eaabc87f2d5c78bf103
manoj <manojlakshan9991@gmail.com>
2023-02-13 18:56:01 -0800

    devicePreview

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:project_bloem/screens/login_screen/login.dart';
 //import 'package:project_bloem/screens/register_form/registerform.dart';
 import 'package:project_bloem/screens/welcome_screen/welcome_screen.dart';
-//import 'package:device_preview/device_preview.dart';
-//import 'Screens/login_screen/login.dart';
+import 'package:device_preview/device_preview.dart';
 
-void main() {
-  runApp( const MyApp(),
-  );
-}
+
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -18,10 +22,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        brightness: Brightness.dark,
+        //brightness: Brightness.dark,
         primarySwatch: Colors.blue,
       ),
-      home : const WelcomeScreen(),
+      home : LoginPage(),
     );
   }
 }
