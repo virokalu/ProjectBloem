c812d3ee4640df84400e3ba8c9403f9b7a03595e
Viro <virokemin@gmail.com>
2023-02-15 23:49:14 +0530

    ListingHalfWay

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
       ),
 
 
-      home : const PlaceListing(),
+      home : PlaceListing(),
 
     );
 
--- lib/screens/place_a_listing/place_a_listing.dart
+++ lib/screens/place_a_listing/place_a_listing.dart
@@ -5,13 +5,28 @@ import '../../components/button_components.dart';
 import '../../components/color_components.dart';
 import '../../components/size.dart';
 
-class PlaceListing extends StatelessWidget {
+var _currencies = [
+  "Food",
+  "Transport",
+  "Personal",
+  "Shopping",
+  "Medical",
+  "Rent",
+  "Movie",
+  "Salary"
+];
 
-
-  const PlaceListing({super.key});
+class PlaceListing extends StatefulWidget {
+  const PlaceListing({Key? key}) : super(key: key);
 
   @override
+  State<PlaceListing> createState() => _PlaceListingState();
+}
+
+class _PlaceListingState extends State<PlaceListing> {
+  @override
   Widget build(BuildContext context) {
+    String currentSelectedValue=_currencies[0];
     var size = MediaQuery.of(context).size;
     var height = size.height;
     var width = size.width;
@@ -34,69 +49,131 @@ class PlaceListing extends StatelessWidget {
                   fontWeight: FontWeight.w600,
                 ),
               ),
+              SizedBox(height: height/100),
               SizedBox(
-                height: height/10,
+                height: height/12,
                 child: TextFormField(
                   decoration: InputDecoration(
                       labelText: "Title",
                       filled: true,
                       fillColor: HexColor.fromHex('#F3F1F1'),
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: BorderSide.none
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none
                       )
                   ),
 
                 ),
               ),
 
+              Text(
+                "Add Description",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(18),
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: height/100),
               SizedBox(
-                height: height/10,
+                height: height/12,
                 child: TextFormField(
+
                   decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.person_pin_sharp),
-                      labelText: "Full Name",
+                      labelText: "Description",
                       filled: true,
-                      fillColor: Colors.white38,
+                      fillColor: HexColor.fromHex('#F3F1F1'),
                       border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
+                          borderRadius: BorderRadius.circular(20),
+                          borderSide: BorderSide.none
                       )
                   ),
 
                 ),
               ),
 
+              Text(
+                "Select the Category",
+                style: TextStyle(
+                  fontSize: getProportionateScreenWidth(18),
+                  color: Colors.black,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(height: height/100),
+              // SizedBox(
+              //   width: width/3,
+              //   height: height/12,
+              //   child: DropdownButton<String>(
+              //
+              //     value: list.first,
+              //     icon: const Icon(Icons.expand_more),
+              //     elevation: 18,
+              //     style: const TextStyle(color: Colors.grey),
+              //     // underline: Container(
+              //     //   height: 2,
+              //     //   color: Colors.deepPurpleAccent,
+              //     // ),
+              //
+              //     dropdownColor: HexColor.fromHex('#F3F1F1'),
+              //     borderRadius: BorderRadius.circular(20),
+              //     onChanged: (String? value) {
+              //       // This is called when the user selects an item.
+              //       // setState(() {
+              //       //   dropdownValue = value!;
+              //       // });
+              //     },
+              //     items: list.map<DropdownMenuItem<String>>((String value) {
+              //       return DropdownMenuItem<String>(
+              //         value: value,
+              //         child: Text(value),
+              //       );
+              //     }).toList(),
+              //   ),
+              // ),
               SizedBox(
-                height: height/10,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.mail_outline_rounded),
-                      labelText: "Email",
-                      filled: true,
-                      fillColor: Colors.white38,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      )
-                  ),
 
+                height: height/10,
+                child: FormField<String>(
+                  builder: (FormFieldState<String> state) {
+                    return InputDecorator(
+                      decoration: InputDecoration(
+                          filled: true,
+                          fillColor: HexColor.fromHex('#F3F1F1'),
+                          labelText: "category",
+                          //labelStyle: textStyle,
+                          //errorStyle: TextStyle(color: Colors.redAccent, fontSize: 16.0),
+                          hintText: 'Please select expense',
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                              borderSide: BorderSide.none
+
+                          )
+                      ),
+                      isEmpty: currentSelectedValue == '',
+                      child: DropdownButtonHideUnderline(
+                        child: DropdownButton<String>(
+                          value: currentSelectedValue,
+                          isDense: true,
+                          onChanged: (String? value) {
+                            // This is called when the user selects an item.
+                            setState(() {
+                              currentSelectedValue = value!;
+                            });
+                          },
+                          items: _currencies.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
 
-              SizedBox(
-                height: height/10,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      prefixIcon: const Icon(Icons.lock),
-                      labelText: "password",
-                      filled: true,
-                      fillColor: Colors.white38,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                      )
-                  ),
-
-                ),
-              ),
 
               SizedBox(
                 height: height/10,
