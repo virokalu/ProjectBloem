e512776605e77b8bd368d00d04148b17963ea285
manoj <manojlakshan9991@gmail.com>
2023-03-09 04:44:07 -0800

    profile screen update

--- lib/screens/user_profile/iconwith_text.dart
+++ lib/screens/user_profile/iconwith_text.dart
@@ -1,5 +1,4 @@
 import "package:flutter/material.dart";
-//import "package:project_bloem/screens/homo_screen/home_screen.dart";
 
 class IconWithText extends StatelessWidget {
   
@@ -13,32 +12,40 @@ class IconWithText extends StatelessWidget {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     var width = size.width;
-    return Row(
-        children: [
-          Expanded(
-           child: IconButton(
-              onPressed: (){
-                  if(icon == Icons.monitor_heart_rounded){Navigator.pushNamed(context, '/mybusket');}
-                  else if(icon == Icons.sell){Navigator.pushNamed(context, '/selling');}
-                  else if(icon == Icons.chat){Navigator.pushNamed(context, '/chat');}
-                  else if(icon == Icons.settings){Navigator.pushNamed(context, '/setting');}
-                  else if(icon == Icons.help){Navigator.pushNamed(context, '/help');}
-                  else if(icon == Icons.error){Navigator.pushNamed(context, '/about us');}
-                  else if(icon == Icons.logout){Navigator.pushNamed(context, '/login');}
-               },
-              icon: Icon(icon),
-           ),
-          ),
-          SizedBox(width: width/3,),
-          Expanded(
-            child: Text(
-              text,
-             // onPressed : (){},
+    return Container(
+      height: 60,
+      child: InkWell(
+        onTap: () {
+                    if(icon == Icons.monitor_heart_rounded){Navigator.pushNamed(context, '/mybusket');}
+                    else if(icon == Icons.sell){Navigator.pushNamed(context, '/selling');}
+                    else if(icon == Icons.chat){
+                      Navigator.pushNamed(context, '/chat');
+                    }
+                    else if(icon == Icons.settings){Navigator.pushNamed(context, '/setting');}
+                    else if(icon == Icons.help){Navigator.pushNamed(context, '/help');}
+                    else if(icon == Icons.error){Navigator.pushNamed(context, '/about us');}
+                    else if(icon == Icons.logout){Navigator.pushNamed(context, '/login');}
+        },
+        child: Row(
+            children: [
+              Expanded(
+               child: Icon(
+                  icon,
+                  size: 40,
+               ),
               ),
-          ),
-         
-        ],
-
+              SizedBox(width: width/3,),
+              Expanded(
+                child: Text(
+                  text,
+                 // onPressed : (){},
+                  ),
+              ),
+             
+            ],
+      
+        ),
+      ),
     );
   }
 }
