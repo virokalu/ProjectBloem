e5c683f889fc47b62ef46d6c0c3a1f3cf7497099
manoj <manojlakshan9991@gmail.com>
2023-04-08 07:01:34 -0700

    warning fixed

--- lib/models/config.dart
+++ lib/models/config.dart
@@ -1,5 +1,5 @@
-const url = 'http://10.0.2.2:3000/';
-//const url = 'http://localhost:3000/';
+//const url = 'http://10.0.2.2:3000/';
+const url = 'http://localhost:3000/';
 const registration = "${url}registration";
 const login = "${url}login";
 const profileImg = "${url}profile/img";
