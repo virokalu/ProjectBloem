69b50e14e8d138ffde24c6ad038d41de0e41a9ff
Viro <virokemin@gmail.com>
2023-04-17 22:03:44 +0530

    ItemViewCompleteInHomePageNew

---
+++ lib/models/item.freezed.dart
@@ -0,0 +1,365 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'item.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+Item _$ItemFromJson(Map<String, dynamic> json) {
+  return _Item.fromJson(json);
+}
+
+/// @nodoc
+mixin _$Item {
+  String get username => throw _privateConstructorUsedError;
+  String get category => throw _privateConstructorUsedError;
+  String get commonname => throw _privateConstructorUsedError;
+  String? get sciname => throw _privateConstructorUsedError;
+  double get price => throw _privateConstructorUsedError;
+  String get description => throw _privateConstructorUsedError;
+  bool get cashondelivery => throw _privateConstructorUsedError;
+  bool get chatactivate => throw _privateConstructorUsedError;
+  String get imgone => throw _privateConstructorUsedError;
+  String get imgtwo => throw _privateConstructorUsedError;
+  String get imgthree => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $ItemCopyWith<Item> get copyWith => throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ItemCopyWith<$Res> {
+  factory $ItemCopyWith(Item value, $Res Function(Item) then) =
+      _$ItemCopyWithImpl<$Res, Item>;
+  @useResult
+  $Res call(
+      {String username,
+      String category,
+      String commonname,
+      String? sciname,
+      double price,
+      String description,
+      bool cashondelivery,
+      bool chatactivate,
+      String imgone,
+      String imgtwo,
+      String imgthree});
+}
+
+/// @nodoc
+class _$ItemCopyWithImpl<$Res, $Val extends Item>
+    implements $ItemCopyWith<$Res> {
+  _$ItemCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? username = null,
+    Object? category = null,
+    Object? commonname = null,
+    Object? sciname = freezed,
+    Object? price = null,
+    Object? description = null,
+    Object? cashondelivery = null,
+    Object? chatactivate = null,
+    Object? imgone = null,
+    Object? imgtwo = null,
+    Object? imgthree = null,
+  }) {
+    return _then(_value.copyWith(
+      username: null == username
+          ? _value.username
+          : username // ignore: cast_nullable_to_non_nullable
+              as String,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      commonname: null == commonname
+          ? _value.commonname
+          : commonname // ignore: cast_nullable_to_non_nullable
+              as String,
+      sciname: freezed == sciname
+          ? _value.sciname
+          : sciname // ignore: cast_nullable_to_non_nullable
+              as String?,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as double,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      cashondelivery: null == cashondelivery
+          ? _value.cashondelivery
+          : cashondelivery // ignore: cast_nullable_to_non_nullable
+              as bool,
+      chatactivate: null == chatactivate
+          ? _value.chatactivate
+          : chatactivate // ignore: cast_nullable_to_non_nullable
+              as bool,
+      imgone: null == imgone
+          ? _value.imgone
+          : imgone // ignore: cast_nullable_to_non_nullable
+              as String,
+      imgtwo: null == imgtwo
+          ? _value.imgtwo
+          : imgtwo // ignore: cast_nullable_to_non_nullable
+              as String,
+      imgthree: null == imgthree
+          ? _value.imgthree
+          : imgthree // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$_ItemCopyWith<$Res> implements $ItemCopyWith<$Res> {
+  factory _$$_ItemCopyWith(_$_Item value, $Res Function(_$_Item) then) =
+      __$$_ItemCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String username,
+      String category,
+      String commonname,
+      String? sciname,
+      double price,
+      String description,
+      bool cashondelivery,
+      bool chatactivate,
+      String imgone,
+      String imgtwo,
+      String imgthree});
+}
+
+/// @nodoc
+class __$$_ItemCopyWithImpl<$Res> extends _$ItemCopyWithImpl<$Res, _$_Item>
+    implements _$$_ItemCopyWith<$Res> {
+  __$$_ItemCopyWithImpl(_$_Item _value, $Res Function(_$_Item) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? username = null,
+    Object? category = null,
+    Object? commonname = null,
+    Object? sciname = freezed,
+    Object? price = null,
+    Object? description = null,
+    Object? cashondelivery = null,
+    Object? chatactivate = null,
+    Object? imgone = null,
+    Object? imgtwo = null,
+    Object? imgthree = null,
+  }) {
+    return _then(_$_Item(
+      username: null == username
+          ? _value.username
+          : username // ignore: cast_nullable_to_non_nullable
+              as String,
+      category: null == category
+          ? _value.category
+          : category // ignore: cast_nullable_to_non_nullable
+              as String,
+      commonname: null == commonname
+          ? _value.commonname
+          : commonname // ignore: cast_nullable_to_non_nullable
+              as String,
+      sciname: freezed == sciname
+          ? _value.sciname
+          : sciname // ignore: cast_nullable_to_non_nullable
+              as String?,
+      price: null == price
+          ? _value.price
+          : price // ignore: cast_nullable_to_non_nullable
+              as double,
+      description: null == description
+          ? _value.description
+          : description // ignore: cast_nullable_to_non_nullable
+              as String,
+      cashondelivery: null == cashondelivery
+          ? _value.cashondelivery
+          : cashondelivery // ignore: cast_nullable_to_non_nullable
+              as bool,
+      chatactivate: null == chatactivate
+          ? _value.chatactivate
+          : chatactivate // ignore: cast_nullable_to_non_nullable
+              as bool,
+      imgone: null == imgone
+          ? _value.imgone
+          : imgone // ignore: cast_nullable_to_non_nullable
+              as String,
+      imgtwo: null == imgtwo
+          ? _value.imgtwo
+          : imgtwo // ignore: cast_nullable_to_non_nullable
+              as String,
+      imgthree: null == imgthree
+          ? _value.imgthree
+          : imgthree // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$_Item implements _Item {
+  _$_Item(
+      {required this.username,
+      required this.category,
+      required this.commonname,
+      this.sciname,
+      required this.price,
+      required this.description,
+      required this.cashondelivery,
+      required this.chatactivate,
+      required this.imgone,
+      required this.imgtwo,
+      required this.imgthree});
+
+  factory _$_Item.fromJson(Map<String, dynamic> json) => _$$_ItemFromJson(json);
+
+  @override
+  final String username;
+  @override
+  final String category;
+  @override
+  final String commonname;
+  @override
+  final String? sciname;
+  @override
+  final double price;
+  @override
+  final String description;
+  @override
+  final bool cashondelivery;
+  @override
+  final bool chatactivate;
+  @override
+  final String imgone;
+  @override
+  final String imgtwo;
+  @override
+  final String imgthree;
+
+  @override
+  String toString() {
+    return 'Item(username: $username, category: $category, commonname: $commonname, sciname: $sciname, price: $price, description: $description, cashondelivery: $cashondelivery, chatactivate: $chatactivate, imgone: $imgone, imgtwo: $imgtwo, imgthree: $imgthree)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_Item &&
+            (identical(other.username, username) ||
+                other.username == username) &&
+            (identical(other.category, category) ||
+                other.category == category) &&
+            (identical(other.commonname, commonname) ||
+                other.commonname == commonname) &&
+            (identical(other.sciname, sciname) || other.sciname == sciname) &&
+            (identical(other.price, price) || other.price == price) &&
+            (identical(other.description, description) ||
+                other.description == description) &&
+            (identical(other.cashondelivery, cashondelivery) ||
+                other.cashondelivery == cashondelivery) &&
+            (identical(other.chatactivate, chatactivate) ||
+                other.chatactivate == chatactivate) &&
+            (identical(other.imgone, imgone) || other.imgone == imgone) &&
+            (identical(other.imgtwo, imgtwo) || other.imgtwo == imgtwo) &&
+            (identical(other.imgthree, imgthree) ||
+                other.imgthree == imgthree));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(
+      runtimeType,
+      username,
+      category,
+      commonname,
+      sciname,
+      price,
+      description,
+      cashondelivery,
+      chatactivate,
+      imgone,
+      imgtwo,
+      imgthree);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ItemCopyWith<_$_Item> get copyWith =>
+      __$$_ItemCopyWithImpl<_$_Item>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$_ItemToJson(
+      this,
+    );
+  }
+}
+
+abstract class _Item implements Item {
+  factory _Item(
+      {required final String username,
+      required final String category,
+      required final String commonname,
+      final String? sciname,
+      required final double price,
+      required final String description,
+      required final bool cashondelivery,
+      required final bool chatactivate,
+      required final String imgone,
+      required final String imgtwo,
+      required final String imgthree}) = _$_Item;
+
+  factory _Item.fromJson(Map<String, dynamic> json) = _$_Item.fromJson;
+
+  @override
+  String get username;
+  @override
+  String get category;
+  @override
+  String get commonname;
+  @override
+  String? get sciname;
+  @override
+  double get price;
+  @override
+  String get description;
+  @override
+  bool get cashondelivery;
+  @override
+  bool get chatactivate;
+  @override
+  String get imgone;
+  @override
+  String get imgtwo;
+  @override
+  String get imgthree;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ItemCopyWith<_$_Item> get copyWith => throw _privateConstructorUsedError;
+}
---
+++ lib/models/item_filter.freezed.dart
@@ -0,0 +1,165 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'item_filter.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#custom-getters-and-methods');
+
+/// @nodoc
+mixin _$ItemFilterModel {
+  PaginationModel get paginationModel => throw _privateConstructorUsedError;
+  String? get categoryId => throw _privateConstructorUsedError;
+
+  @JsonKey(ignore: true)
+  $ItemFilterModelCopyWith<ItemFilterModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $ItemFilterModelCopyWith<$Res> {
+  factory $ItemFilterModelCopyWith(
+          ItemFilterModel value, $Res Function(ItemFilterModel) then) =
+      _$ItemFilterModelCopyWithImpl<$Res, ItemFilterModel>;
+  @useResult
+  $Res call({PaginationModel paginationModel, String? categoryId});
+
+  $PaginationModelCopyWith<$Res> get paginationModel;
+}
+
+/// @nodoc
+class _$ItemFilterModelCopyWithImpl<$Res, $Val extends ItemFilterModel>
+    implements $ItemFilterModelCopyWith<$Res> {
+  _$ItemFilterModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? paginationModel = null,
+    Object? categoryId = freezed,
+  }) {
+    return _then(_value.copyWith(
+      paginationModel: null == paginationModel
+          ? _value.paginationModel
+          : paginationModel // ignore: cast_nullable_to_non_nullable
+              as PaginationModel,
+      categoryId: freezed == categoryId
+          ? _value.categoryId
+          : categoryId // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ) as $Val);
+  }
+
+  @override
+  @pragma('vm:prefer-inline')
+  $PaginationModelCopyWith<$Res> get paginationModel {
+    return $PaginationModelCopyWith<$Res>(_value.paginationModel, (value) {
+      return _then(_value.copyWith(paginationModel: value) as $Val);
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$$_ItemFilterModelCopyWith<$Res>
+    implements $ItemFilterModelCopyWith<$Res> {
+  factory _$$_ItemFilterModelCopyWith(
+          _$_ItemFilterModel value, $Res Function(_$_ItemFilterModel) then) =
+      __$$_ItemFilterModelCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({PaginationModel paginationModel, String? categoryId});
+
+  @override
+  $PaginationModelCopyWith<$Res> get paginationModel;
+}
+
+/// @nodoc
+class __$$_ItemFilterModelCopyWithImpl<$Res>
+    extends _$ItemFilterModelCopyWithImpl<$Res, _$_ItemFilterModel>
+    implements _$$_ItemFilterModelCopyWith<$Res> {
+  __$$_ItemFilterModelCopyWithImpl(
+      _$_ItemFilterModel _value, $Res Function(_$_ItemFilterModel) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? paginationModel = null,
+    Object? categoryId = freezed,
+  }) {
+    return _then(_$_ItemFilterModel(
+      paginationModel: null == paginationModel
+          ? _value.paginationModel
+          : paginationModel // ignore: cast_nullable_to_non_nullable
+              as PaginationModel,
+      categoryId: freezed == categoryId
+          ? _value.categoryId
+          : categoryId // ignore: cast_nullable_to_non_nullable
+              as String?,
+    ));
+  }
+}
+
+/// @nodoc
+
+class _$_ItemFilterModel implements _ItemFilterModel {
+  _$_ItemFilterModel({required this.paginationModel, this.categoryId});
+
+  @override
+  final PaginationModel paginationModel;
+  @override
+  final String? categoryId;
+
+  @override
+  String toString() {
+    return 'ItemFilterModel(paginationModel: $paginationModel, categoryId: $categoryId)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$_ItemFilterModel &&
+            (identical(other.paginationModel, paginationModel) ||
+                other.paginationModel == paginationModel) &&
+            (identical(other.categoryId, categoryId) ||
+                other.categoryId == categoryId));
+  }
+
+  @override
+  int get hashCode => Object.hash(runtimeType, paginationModel, categoryId);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$_ItemFilterModelCopyWith<_$_ItemFilterModel> get copyWith =>
+      __$$_ItemFilterModelCopyWithImpl<_$_ItemFilterModel>(this, _$identity);
+}
+
+abstract class _ItemFilterModel implements ItemFilterModel {
+  factory _ItemFilterModel(
+      {required final PaginationModel paginationModel,
+      final String? categoryId}) = _$_ItemFilterModel;
+
+  @override
+  PaginationModel get paginationModel;
+  @override
+  String? get categoryId;
+  @override
+  @JsonKey(ignore: true)
+  _$$_ItemFilterModelCopyWith<_$_ItemFilterModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
