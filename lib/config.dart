4d67ca4bf8850a2c08a58e2a00543ff97f443951
Viro <virokemin@gmail.com>
2023-04-18 22:31:32 +0530

    LinkAdded

--- lib/config.dart
+++ lib/config.dart
@@ -7,6 +7,7 @@ const profileImg = "${url}profile/img";
 const profileGetImg = "${url}profile/getimg";
 const itemAdd = "${url}item";
 const apiURL = "10.0.2.2:3000";
+//const apiURL = "localhost:3000";
 const itemGet = "item";
 const String appName = "Project Bloem";
 const int pageSize = 10;
