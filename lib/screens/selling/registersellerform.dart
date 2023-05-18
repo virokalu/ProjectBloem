51098f55c333ef2e40a6286a92daaea723b93b94
manoj <manojlakshan9991@gmail.com>
2023-05-18 10:36:57 -0700

    sellerRegister

---
+++ lib/screens/selling/registersellerform.dart
@@ -0,0 +1,18 @@
+import "package:flutter/material.dart";
+
+class SellerRegister extends StatefulWidget {
+  const SellerRegister({super.key});
+
+  @override
+  State<SellerRegister> createState() => _SellerRegisterState();
+}
+
+class _SellerRegisterState extends State<SellerRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () {}, 
+      child: const Text("Press"),
+    );
+  }
+}
