d1d7a433b17e0d12dc8a7f2daa2150eecd41a97f
SherinShashika <sherinfernando02@gmail.com>
2023-05-26 12:49:13 +0530

    term and conditions page updated

--- lib/screens/t&c/term_cond.dart
+++ lib/screens/t&c/term_cond.dart
@@ -35,117 +35,119 @@ class _TermAndConditionState extends State<TermAndCondition> {
             Text(
               'Welcome to our m-commerce app. By accessing and using this app, you agree to be bound by the following terms and conditions. Please read them carefully',
               textAlign: TextAlign.justify,
+              style: TextStyle(fontSize: 15),
             ),
             SizedBox(
               height: 16.0,
             ),
             Text(
               '1. User Responsibilities:',
-              style: TextStyle(fontWeight: FontWeight.bold),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
             ),
             Text(
               '- You are responsible for providing accurate and complete registration information.',
+              style: TextStyle(fontSize: 12),
             ),
             Text(
-              '- You must use your personal account in a lawful manner and comply with all applicable laws and regulations.',
-            ),
+                '- You must use your personal account in a lawful manner and comply with all applicable laws and regulations.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- Unauthorized access to or tampering with the app is strictly prohibited.',
-            ),
+                '- Unauthorized access to or tampering with the app is strictly prohibited.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '2. Account Creation and Security:',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                fontSize: 13,
               ),
             ),
             Text(
-              '- You are solely responsible for creating a strong password and safeguarding your login credentials.',
-            ),
+                '- You are solely responsible for creating a strong password and safeguarding your login credentials.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- Notify us immediately of any unauthorized access or suspicious activity related to your account.',
-            ),
+                '- Notify us immediately of any unauthorized access or suspicious activity related to your account.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '3. Intellectual Property Rights:',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                fontSize: 13,
               ),
             ),
             Text(
-              '- All intellectual property rights within the app, including trademarks, copyrights, and patents, belong to us.',
-            ),
+                '- All intellectual property rights within the app, including trademarks, copyrights, and patents, belong to us.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- You may not use, reproduce, or distribute any app content without our explicit permission.',
-            ),
+                '- You may not use, reproduce, or distribute any app content without our explicit permission.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '4. Payment and Billing:',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                fontSize: 13,
               ),
             ),
             Text(
-              '- We offer various payment methods, such as credit cards, cash on delivery',
-            ),
+                '- We offer various payment methods, such as credit cards, cash on delivery',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- Payments will be processed according to the selected method and any applicable fees.',
-            ),
+                '- Payments will be processed according to the selected method and any applicable fees.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '5. Transaction Fees:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
             ),
             Text(
-              '- For the first \$5000 received by the seller, no transaction fees will be charged.',
-            ),
+                '- For the first \$5000 received by the seller, no transaction fees will be charged.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- For any amount received by the seller above \$5000, a 5% transaction fee will be deducted.',
-            ),
+                '- For any amount received by the seller above \$5000, a 5% transaction fee will be deducted.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '6. Product Listings and Descriptions:',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                fontSize: 13,
               ),
             ),
             Text(
-              '- Our app may include product listings or descriptions provided by sellers or third parties.',
-            ),
+                '- Our app may include product listings or descriptions provided by sellers or third parties.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- We do not guarantee the accuracy, quality, or legality of the products or services offered by sellers.',
-            ),
+                '- We do not guarantee the accuracy, quality, or legality of the products or services offered by sellers.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '7. Product Delivery:',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
+                fontSize: 13,
               ),
             ),
             Text(
-              '- We provide the information of delivery companies for your convenience.',
-            ),
+                '- We provide the information of delivery companies for your convenience.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- We do not take any responsibility for the delivery of products.',
-            ),
-            Text(
-              '- Use of these delivery services is at your own risk.',
-            ),
+                '- We do not take any responsibility for the delivery of products.',
+                style: TextStyle(fontSize: 12)),
+            Text('- Use of these delivery services is at your own risk.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Text(
               '8. Termination of Account:',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
             ),
             Text(
-              '- We reserve the right to terminate or suspend your account in case of violation of the terms or illegal activities.',
-            ),
+                '- We reserve the right to terminate or suspend your account in case of violation of the terms or illegal activities.',
+                style: TextStyle(fontSize: 12)),
             Text(
-              '- Termination may result in the loss of access to the app and any associated data.',
-            ),
+                '- Termination may result in the loss of access to the app and any associated data.',
+                style: TextStyle(fontSize: 12)),
             SizedBox(height: 16.0),
             Row(children: [
               Checkbox(
@@ -157,7 +159,8 @@ class _TermAndConditionState extends State<TermAndCondition> {
                   }),
               Expanded(
                   child: Text(
-                      'I have read and agree to the terms and conditions.'))
+                      'I have read and agree to the terms and conditions.',
+                      style: TextStyle(fontSize: 12)))
             ]),
             Container(
               margin: EdgeInsets.only(top: 10),
